ef6bdb9bba32420c049759cd79ae8163ed8abb75
nicodeheza <nicolasdeheza@hotmail.com>
2023-07-12 19:49:25 -0300

    p5 component

---
+++ src/components/P5Canvas.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type P5 from 'p5';
+	import { onMount } from 'svelte';
+	export let sketch: (p5: P5) => void;
+
+	let element: HTMLDivElement;
+
+	onMount(async () => {
+		const lib = await import('p5');
+		const { default: P5 } = lib;
+		if (!element && window) return;
+		new P5(sketch, element);
+	});
+</script>
+
+<div bind:this={element} />
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import P5Canvas from '../../components/P5Canvas.svelte';
+	import type P5 from 'p5';
+
+	const sketch = (p5: P5) => {
+		p5.setup = () => {
+			p5.createCanvas(100, 100);
+			p5.background(0);
+		};
+	};
+</script>
+
+<P5Canvas {sketch} />
