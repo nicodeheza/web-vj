2871863be138fb0cfe78c2cce32891b93c70af79
nicodeheza <nicolasdeheza@hotmail.com>
2023-07-14 19:09:44 -0300

    remove semi

--- src/lib/appComponents/types/index.ts
+++ src/lib/appComponents/types/index.ts
@@ -1,17 +1,17 @@
-import type p5 from 'p5';
+import type p5 from 'p5'
 
 export interface AssetComponentI {
-	p5: p5;
-	img: p5.Image | undefined;
-	width: number;
-	height: number;
-	x: number;
-	y: number;
-	rotation: number;
-	scale: number;
-	uri: string;
-	output: p5.Graphics | undefined;
-	preload: () => void;
-	setup: () => void;
-	display: () => void;
+	p5: p5
+	img: p5.Image | undefined
+	width: number
+	height: number
+	x: number
+	y: number
+	rotation: number
+	scale: number
+	uri: string
+	output: p5.Graphics | undefined
+	preload: () => void
+	setup: () => void
+	display: () => void
 }
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,41 +1,41 @@
 <script lang="ts">
-	import P5Canvas from '../../components/P5Canvas.svelte';
-	import type P5 from 'p5';
+	import P5Canvas from '../../components/P5Canvas.svelte'
+	import type P5 from 'p5'
 
-	let posX = 100;
-	let posY = 100;
-	let scale = 100;
-	let rot = 0;
-	let pivotX = 0;
-	let pivotY = 0;
+	let posX = 100
+	let posY = 100
+	let scale = 100
+	let rot = 0
+	let pivotX = 0
+	let pivotY = 0
 
 	const sketch = (p5: P5) => {
-		let testShader: P5.Shader;
-		let img: P5.Image;
+		let testShader: P5.Shader
+		let img: P5.Image
 		p5.preload = () => {
-			testShader = p5.loadShader('shaders/test/test.vert', 'shaders/test/test.frag');
-			img = p5.loadImage('img/test.jpg');
-		};
+			testShader = p5.loadShader('shaders/test/test.vert', 'shaders/test/test.frag')
+			img = p5.loadImage('img/test.jpg')
+		}
 		p5.setup = () => {
-			p5.pixelDensity(1);
-			p5.createCanvas(600, 600, p5.WEBGL);
-			p5.noStroke();
-			img.resize(p5.width, p5.height);
-		};
+			p5.pixelDensity(1)
+			p5.createCanvas(600, 600, p5.WEBGL)
+			p5.noStroke()
+			img.resize(p5.width, p5.height)
+		}
 		p5.draw = () => {
-			p5.background(0);
-			p5.shader(testShader);
-			testShader.setUniform('uTexture', img);
-			testShader.setUniform('uXpos', posX / 100 - 1);
-			testShader.setUniform('uYpos', posY / 100 - 1);
-			testShader.setUniform('uScale', (scale * 2) / 100);
-			testShader.setUniform('uRotation', rot);
-			testShader.setUniform('uPivotX', pivotX);
-			testShader.setUniform('uPivotY', pivotY);
-			p5.scale(0.5);
-			p5.rect(0, 0, p5.width);
-		};
-	};
+			p5.background(0)
+			p5.shader(testShader)
+			testShader.setUniform('uTexture', img)
+			testShader.setUniform('uXpos', posX / 100 - 1)
+			testShader.setUniform('uYpos', posY / 100 - 1)
+			testShader.setUniform('uScale', (scale * 2) / 100)
+			testShader.setUniform('uRotation', rot)
+			testShader.setUniform('uPivotX', pivotX)
+			testShader.setUniform('uPivotY', pivotY)
+			p5.scale(0.5)
+			p5.rect(0, 0, p5.width)
+		}
+	}
 </script>
 
 <P5Canvas {sketch} />
